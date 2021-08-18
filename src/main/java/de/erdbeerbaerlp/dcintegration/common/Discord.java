a7b204164c5d914c0bf30b1cdae1487ad4a5156b
ErdbeerbaerLP <27149563+ErdbeerbaerLP@users.noreply.github.com>
2021-08-19 01:29:30 +0200

    Make link codes shorter

--- src/main/java/de/erdbeerbaerlp/dcintegration/common/Discord.java
+++ src/main/java/de/erdbeerbaerlp/dcintegration/common/Discord.java
@@ -632,7 +632,7 @@ public class Discord extends Thread {
         });
         if (r.get() != -1) return r.get();
         do {
-            r.set(new Random().nextInt(Integer.MAX_VALUE));
+            r.set(new Random().nextInt(999999));
         } while (pendingLinks.containsKey(r.get()));
         pendingLinks.put(r.get(), new DefaultKeyValue<>(Instant.now(), uniqueID));
         return r.get();
@@ -652,7 +652,7 @@ public class Discord extends Thread {
         });
         if (r.get() != -1) return r.get();
         do {
-            r.set(new Random().nextInt(99999)); //Making it shorter as bedrock has no click events in chat
+            r.set(new Random().nextInt(99999));
         } while (pendingBedrockLinks.containsKey(r.get()));
         pendingBedrockLinks.put(r.get(), new DefaultKeyValue<>(Instant.now(), uniqueID));
         return r.get();
