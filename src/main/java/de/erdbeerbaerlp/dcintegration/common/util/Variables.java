0cb65f6d919e5331946bd992b10a10b1d060da35
ErdbeerbaerLP <admin@erdbeerbaerlp.de>
2022-06-17 15:05:33 +0200

    Update version number

--- src/main/java/de/erdbeerbaerlp/dcintegration/common/util/Variables.java
+++ src/main/java/de/erdbeerbaerlp/dcintegration/common/util/Variables.java
@@ -13,7 +13,7 @@ public class Variables {
     /**
      * Mod/Plugin version
      */
-    public static final String VERSION = "2.4.2";
+    public static final String VERSION = "2.4.3";
     /**
      * Discord Integration data directory
      */
